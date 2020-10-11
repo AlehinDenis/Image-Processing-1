7232f0214a4ef586951c5a0b5b5bf54715785cc5
Denis <alehin.de@gmail.com>
2020-10-11 12:35:18 +0300

    Similarity of two images is done

---
+++ Image-Processing-1/Image-Processing-1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Image_Processing_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filtresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.symilarityOfTwoImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shapesOfGrayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filtresToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // filtresToolStripMenuItem
+            // 
+            this.filtresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.symilarityOfTwoImagesToolStripMenuItem,
+            this.shapesOfGrayToolStripMenuItem});
+            this.filtresToolStripMenuItem.Name = "filtresToolStripMenuItem";
+            this.filtresToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.filtresToolStripMenuItem.Text = "Filters";
+            // 
+            // symilarityOfTwoImagesToolStripMenuItem
+            // 
+            this.symilarityOfTwoImagesToolStripMenuItem.Name = "symilarityOfTwoImagesToolStripMenuItem";
+            this.symilarityOfTwoImagesToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.symilarityOfTwoImagesToolStripMenuItem.Text = "Similarity of two images";
+            this.symilarityOfTwoImagesToolStripMenuItem.Click += new System.EventHandler(this.symilarityOfTwoImagesToolStripMenuItem_Click);
+            // 
+            // shapesOfGrayToolStripMenuItem
+            // 
+            this.shapesOfGrayToolStripMenuItem.Name = "shapesOfGrayToolStripMenuItem";
+            this.shapesOfGrayToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.shapesOfGrayToolStripMenuItem.Text = "Shapes of gray";
+            this.shapesOfGrayToolStripMenuItem.Click += new System.EventHandler(this.shapesOfGrayToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(760, 422);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(402, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(370, 422);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image processing";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filtresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem symilarityOfTwoImagesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shapesOfGrayToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Image-Processing-1/Image-Processing-1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_Processing_1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image1;
+        Bitmap image2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void symilarityOfTwoImagesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog1 = new OpenFileDialog();
+            dialog1.Filter = "Image files|*.png;*.jpg;*.bmp|All filec(*.*)|*.*";
+            if (dialog1.ShowDialog() == DialogResult.OK)
+            {
+                image1 = new Bitmap(dialog1.FileName);
+                pictureBox1.Image = image1;
+                pictureBox1.Width = 370;
+                pictureBox1.Refresh();
+            }
+
+            OpenFileDialog dialog2 = new OpenFileDialog();
+            dialog2.Filter = "Image files|*.png;*.jpg;*.bmp|All filec(*.*)|*.*";
+            if (dialog2.ShowDialog() == DialogResult.OK)
+            {
+                image2 = new Bitmap(dialog2.FileName);
+                pictureBox2.Image = image2;
+                pictureBox2.Show();
+                pictureBox2.Refresh();
+                
+            }
+
+            if(image1.Width != image2.Width || image1.Height != image2.Height)
+            {
+                MessageBox.Show("Image are not the same size!");
+                return;
+            }
+
+            double mseR = 0;
+            double mseG = 0;
+            double mseB = 0;
+
+            for (int i = 0; i < image1.Width; i++)
+                for (int j = 0; j < image1.Height; j++)
+                {
+                    mseR += Math.Pow(image1.GetPixel(i, j).R - image2.GetPixel(i, j).R, 2);
+                    mseG += Math.Pow(image1.GetPixel(i, j).G - image2.GetPixel(i, j).G, 2);
+                    mseB += Math.Pow(image1.GetPixel(i, j).B - image2.GetPixel(i, j).B, 2);
+                }
+
+            int countOfPixels = (image1.Width - 1) * (image1.Height - 1);
+            mseR /= countOfPixels;
+            mseG /= countOfPixels;
+            mseB /= countOfPixels;
+
+            double mse = mseR + mseG + mseB;
+            MessageBox.Show("MSE is " + mse);
+        }
+
+        private void shapesOfGrayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog1 = new OpenFileDialog();
+            dialog1.Filter = "Image files|*.png;*.jpg;*.bmp|All filec(*.*)|*.*";
+            if (dialog1.ShowDialog() == DialogResult.OK)
+            {
+                image1 = new Bitmap(dialog1.FileName);
+                pictureBox1.Image = image1;
+                pictureBox1.Width = 760;
+                pictureBox2.Hide();
+                pictureBox1.Refresh();
+            }
+        }
+    }
+}
